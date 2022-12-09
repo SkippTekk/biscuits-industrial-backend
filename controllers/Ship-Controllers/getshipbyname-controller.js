761cb580c2d00d148033e3ae772dd97695dac080
atquick <atquick@outlook.com>
2022-12-08 21:50:50 -0500

    combined ship and material query to combine data into one json object, still need to add sanitization and validation to route

--- controllers/Ship-Controllers/getshipbyname-controller.js
+++ controllers/Ship-Controllers/getshipbyname-controller.js
@@ -3,16 +3,60 @@ const db = require("../../db/index");
 const getShipByName = async (req, res) => {
   const input = req.params.input;
 
+  let shipData = [];
+
   try {
     db.query(
-      `SELECT * FROM invTypes
-        INNER JOIN invGroups
-        ON invGroups.groupID=invTypes.groupID
-        INNER JOIN (select marketGroupID, parentGroupID, marketGroupName, description as marketDesc, iconID, hasTypes from invMarketGroups) invMarketGroups
-        ON invMarketGroups.marketGroupID = invTypes.marketGroupID
-        AND invGroups.categoryID = 6
-        AND invTypes.published = 1
-        AND invTypes.typeName = "${input}"
+      `SELECT 
+      invTypes.typeID,
+      invTypes.groupID,
+      invTypes.typeName,
+      invTypes.description,
+      invTypes.mass,
+      invTypes.volume,
+      invTypes.capacity,
+      invTypes.marketGroupID,
+      invTypes.basePrice,
+      invMarketGroups.marketGroupName,
+      invGroups.groupName
+  FROM
+      invTypes
+          JOIN
+      (SELECT 
+          groupID, categoryID, groupName
+      FROM
+          invGroups) invGroups ON invGroups.groupID = invTypes.groupID
+          JOIN
+      (SELECT 
+          marketGroupID, marketGroupName
+      FROM
+          invMarketGroups) invMarketGroups ON invMarketGroups.marketGroupID = invTypes.marketGroupID
+          AND invGroups.categoryID = 6
+          AND invTypes.published = 1
+          AND invTypes.typeName = "${input}"`,
+      (err, data) => {
+        if (err) {
+          return res.status(404).json({
+            error: "Unable to locate that item within database.",
+          });
+        } else {
+          shipData.push({ ship: data[0] });
+        }
+      }
+    );
+
+    db.query(
+      `SELECT m.materialTypeID, m.quantity, i2.typeName, m.activityID 
+      FROM industryActivityMaterials m 
+      INNER JOIN invTypes i1 
+      ON i1.typeID = m.typeID 
+      INNER JOIN invTypes i2 
+      ON i2.typeID = m.materialtypeID 
+      INNER JOIN ramActivities i3 
+      ON i3.activityID = m.activityID = 1 
+      WHERE i1.typeName = "${input} Blueprint"
+      AND m.activityID = 1 
+      ORDER BY m.materialTypeID ASC
       `,
       (err, data) => {
         if (err) {
@@ -20,7 +64,8 @@ const getShipByName = async (req, res) => {
             .status(404)
             .json({ error: "Unable to locate that item within database." });
         } else {
-          return res.status(200).json(data);
+          shipData.push({ materials: data });
+          return res.json(shipData);
         }
       }
     );
--- routes/ship-routes.js
+++ routes/ship-routes.js
@@ -2,9 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const getShipByName = require("../controllers/Ship-Controllers/getshipbyname-controller");
-const getShipMaterials = require("../controllers/Ship-Controllers/getshipmaterials-controller");
 
 router.route("/getShipByName/:input").get(getShipByName);
-router.route("/getShipMaterials/:input").get(getShipMaterials);
 
 module.exports = router;
