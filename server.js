0ee51e60e0b286a6c358eefce13e44ab855bb6fe
atquick <atquick@outlook.com>
2022-12-15 22:50:58 -0500

    correcting cors config

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://localhost:3000",
     credentials: false,
     methods: "GET",
     allowedHeaders: [
